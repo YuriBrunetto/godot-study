fedbe2bb2d0d6392eef000e252e7899bb774aa54
CesarNoll <cesar.b.noll@gmail.com>
2018-04-28 23:26:49 -0300

    Mais codigo

--- scripts/Character.gd
+++ scripts/Character.gd
@@ -1,8 +1,13 @@
 extends KinematicBody2D
 
 export(float) var speed = 128
+export(float) var jump_speed = 128
 
 var walking = false
+var velocity = Vector2()
+
+var gravity = 2
+var jumping = false
 
 func _ready():
 	pass
@@ -11,14 +16,19 @@ func _physics_process(delta):
 	walking = false
 	if Input.is_action_pressed("direita"):
 		walking = true
-		$AnimatedSprite.flip_h = false
-		position.x += speed * delta * scale.x
+		velocity.x = 1
 	if Input.is_action_pressed("esquerda"):
 		walking = true
-		$AnimatedSprite.flip_h = true
-		position.x -= speed * delta * scale.x
+		velocity.x = -1
+	if Input.is_action_just_pressed("pular"):
+		velocity.y = -1
+		jumping = true
 	
 	if walking:
 		$AnimatedSprite.play()
+		$AnimatedSprite.flip_h = (sign(velocity.x) == -1)
+		position.x += velocity.x * speed * delta * scale.x
 	else:
 		$AnimatedSprite.stop()
+	
+	velocity 
